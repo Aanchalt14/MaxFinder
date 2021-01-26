4619a50a4ae2acd005e7eea759e9ae35567fb600
Aanchalt14 <aanchalt71@gmail.com>
2021-01-26 23:32:18 +0530

    Add : Find Maximum among String

--- MaxFinder.java
+++ MaxFinder.java
@@ -1,13 +1,23 @@
 package com.uccases;
 
-public class MaxFinder {
-    public int findMax(Integer a, Integer b, Integer c) {
-        int max = a;
-        if (b.compareTo(max) > 0 && b.compareTo(c) > 0 )
+public class MaxFinder<E extends  Comparable<E>> {
+          private E a,b,c;
+
+    public MaxFinder(E a, E b, E c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public E maximum(){
+        return findMaxValue(a,b,c);
+    }
+
+    public static <E extends Comparable> E findMaxValue(E a,E b,E c) {
+        E max =  a;
+        if (b.compareTo(max) > 0 && b.compareTo(c) > 0)
             max = b;
         else if (c.compareTo(max) > 0)
             max = c;
         return max;
     }
 }
-
